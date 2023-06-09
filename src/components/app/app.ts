8bd1a200af1ccc6493ee4c2070336904d9fd4ac4
Laevus92 <laevus@yandex.ru>
2023-06-09 18:45:45 +0300

    feat: migrate app to TS

---
+++ src/components/app/app.ts
@@ -0,0 +1,20 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+    controller: AppController
+    view: AppView
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    public start(): void {
+        document
+            .querySelector('.sources')!
+            .addEventListener('click', (e: Event): void => this.controller.getNews(e, (data): void => this.view.drawNews(data)));
+        this.controller.getSources((data):void => this.view.drawSources(data));
+    }
+}
+
+export default App;
